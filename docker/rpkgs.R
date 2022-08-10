62a67e2d971fed1e269dfcc64f79cf7e807fad61
markziemann <mark.ziemann@gmail.com>
2022-08-10 19:55:46 +1000

    added hrbrthemes

--- docker/rpkgs.R
+++ docker/rpkgs.R
@@ -21,6 +21,8 @@ install.packages("rmarkdown")
 install.packages("knitr")
 install.packages("kableExtra")
 
+install.packages("hrbrthemes")
+
 packageurl <- "https://cran.r-project.org/src/contrib/Archive/rvcheck/rvcheck_0.1.8.tar.gz"
 install.packages(packageurl, repos=NULL, type="source")
 
